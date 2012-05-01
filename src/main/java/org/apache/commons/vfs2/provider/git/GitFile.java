ff6b8f0cc7338534f9c0f316371d03b785c03e8a
mmounirou <mmounirou@mmounirou.com>
2012-05-01 14:37:59 +0200

    implement all method for full file system function

--- src/main/java/org/apache/commons/vfs2/provider/git/GitFile.java
+++ src/main/java/org/apache/commons/vfs2/provider/git/GitFile.java
@@ -1,30 +1,38 @@
 package org.apache.commons.vfs2.provider.git;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileType;
+import org.apache.commons.vfs2.RandomAccessContent;
 import org.apache.commons.vfs2.provider.AbstractFileName;
 import org.apache.commons.vfs2.provider.AbstractFileObject;
 import org.apache.commons.vfs2.provider.UriParser;
+import org.apache.commons.vfs2.provider.local.GitFileRandomAccessContent;
+import org.apache.commons.vfs2.util.RandomAccessMode;
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.treewalk.TreeWalk;
 
-public class GitFile extends AbstractFileObject implements FileObject
-{
+public class GitFile extends AbstractFileObject implements FileObject {
 
 	private final FileName rootName;
 	private final GitFileSystem gitFileSystem;
 
-	public GitFile(GitFileSystem gitFileSystem, FileName rootName, AbstractFileName name)
-	{
+	public GitFile(GitFileSystem gitFileSystem, FileName rootName,
+			AbstractFileName name) {
 		super(name, gitFileSystem);
 		this.rootName = rootName;
 		this.gitFileSystem = gitFileSystem;
@@ -32,10 +40,8 @@ public class GitFile extends AbstractFileObject implements FileObject
 	}
 
 	@Override
-	protected FileType doGetType() throws Exception
-	{
-		if (isRootDir())
-		{
+	protected FileType doGetType() throws Exception {
+		if (isRootDir()) {
 			return FileType.FOLDER;
 		}
 
@@ -43,21 +49,17 @@ public class GitFile extends AbstractFileObject implements FileObject
 		RevTree tree = gitFileSystem.getTree();
 
 		TreeWalk treeWalk = buildTreeWalk(repository, tree);
-		if (treeWalk == null)
-		{
+		if (treeWalk == null) {
 			// The file isn't in the local repository
 			return FileType.IMAGINARY;
-		} else
-		{
+		} else {
 			FileMode fileMode = treeWalk.getFileMode(0);
-			if (fileMode == FileMode.TREE)
-			{
+			if (fileMode == FileMode.TREE) {
 				return FileType.FOLDER;
-			} else if (fileMode == FileMode.EXECUTABLE_FILE || fileMode == FileMode.REGULAR_FILE)
-			{
+			} else if (fileMode == FileMode.EXECUTABLE_FILE
+					|| fileMode == FileMode.REGULAR_FILE) {
 				return FileType.FILE;
-			} else
-			{
+			} else {
 				return FileType.IMAGINARY;
 			}
 
@@ -66,8 +68,7 @@ public class GitFile extends AbstractFileObject implements FileObject
 	}
 
 	@Override
-	protected String[] doListChildren() throws Exception
-	{
+	protected String[] doListChildren() throws Exception {
 		Repository repository = gitFileSystem.getRepository();
 		RevTree tree = gitFileSystem.getTree();
 		TreeWalk treeWalk = buildTreeWalk(repository, tree);
@@ -75,8 +76,7 @@ public class GitFile extends AbstractFileObject implements FileObject
 		treeWalk.enterSubtree();
 		List<String> results = new ArrayList<String>();
 
-		while (treeWalk.next())
-		{
+		while (treeWalk.next()) {
 			String strName = treeWalk.getNameString();
 			results.add(strName);
 		}
@@ -84,30 +84,85 @@ public class GitFile extends AbstractFileObject implements FileObject
 		return UriParser.encode(results.toArray(new String[results.size()]));
 	}
 
-	private TreeWalk buildTreeWalk(Repository repository, RevTree tree) throws IOException
-	{
-		// The treeWalker doesn't work with "." path so consider it as
-		// particular path
-		TreeWalk treeWalk = null;
-		if (isRootDir())
-		{
-			treeWalk = new TreeWalk(repository);
-			treeWalk.addTree(tree);
-		} else
-		{
-			treeWalk = TreeWalk.forPath(repository, rootName.getRelativeName(getName()), tree);
-		}
-		return treeWalk;
-	}
-
-	private boolean isRootDir()
-	{
-		return getName().compareTo(rootName) == 0;
+	@Override
+	protected void doDelete() throws Exception {
+		Repository repository = gitFileSystem.getRepository();
+		Git git = new Git(repository);
+		git.rm().addFilepattern(getRelativePath()).call();
+		git.commit().setMessage(String.format("Delete %s", getRelativePath()));
 	}
 
 	@Override
-	protected long doGetContentSize() throws Exception
-	{
+	protected void doRename(FileObject newfile) throws Exception {
+		Repository repository = gitFileSystem.getRepository();
+		Git git = new Git(repository);
+
+		// remove old file
+		git.rm().addFilepattern(getRelativePath()).call();
+
+		// rename on file system
+		new File(getName().getPathDecoded()).renameTo(new File(newfile
+				.getName().getPathDecoded()));
+
+		// add new file
+		git.add().addFilepattern(rootName.getRelativeName(newfile.getName()));
+
+		git.commit().setMessage(
+				String.format("Rename %s to %s", getRelativePath(),
+						rootName.getRelativeName(newfile.getName())));
+
+	}
+
+	@Override
+	protected void doCreateFolder() throws Exception {
+		new File(getName().getPathDecoded()).mkdir();
+	}
+
+	@Override
+	protected long doGetLastModifiedTime() throws Exception {
+		// TODO modified to return the date of last commit which contains this
+		// file
+		return new File(getName().getPathDecoded()).lastModified();
+	}
+
+	@Override
+	protected boolean doSetLastModifiedTime(long modtime) throws Exception {
+		// TODO modified to support only if the modtime is now.
+		return new File(getName().getPathDecoded()).setLastModified(modtime);
+	}
+
+	@Override
+	protected RandomAccessContent doGetRandomAccessContent(RandomAccessMode mode)
+			throws Exception {
+
+		// TODO find a way to not copy the content fully for random access
+
+		File tempFile = File.createTempFile("plpo", "plpo");
+		tempFile.deleteOnExit();
+
+		InputStream inputStream = doGetInputStream();
+		OutputStream outPutStream = new FileOutputStream(tempFile);
+
+		try {
+			IOUtils.copyLarge(inputStream, outPutStream);
+		} finally {
+			IOUtils.closeQuietly(outPutStream);
+			IOUtils.closeQuietly(inputStream);
+		}
+
+		return new GitFileRandomAccessContent(tempFile, mode);
+	}
+
+	@Override
+	protected OutputStream doGetOutputStream(boolean bAppend) throws Exception {
+		// TODO update to be able to auto commit the file after the stream
+		// close.
+
+		return new FileOutputStream(getName().getPathDecoded(), bAppend);
+	}
+
+	@Override
+	protected long doGetContentSize() throws Exception {
 		Repository repository = gitFileSystem.getRepository();
 		RevTree tree = gitFileSystem.getTree();
 
@@ -117,13 +172,34 @@ public class GitFile extends AbstractFileObject implements FileObject
 	}
 
 	@Override
-	protected InputStream doGetInputStream() throws Exception
-	{
+	protected InputStream doGetInputStream() throws Exception {
 		Repository repository = gitFileSystem.getRepository();
 		RevTree tree = gitFileSystem.getTree();
 
 		TreeWalk treeWalk = buildTreeWalk(repository, tree);
 		ObjectLoader objectLoader = repository.open(treeWalk.getObjectId(0));
 		return objectLoader.openStream();
+	}
+
+	private TreeWalk buildTreeWalk(Repository repository, RevTree tree)
+			throws IOException {
+		// The treeWalker doesn't work with "." path so consider it as
+		// particular path
+		TreeWalk treeWalk = null;
+		if (isRootDir()) {
+			treeWalk = new TreeWalk(repository);
+			treeWalk.addTree(tree);
+		} else {
+			treeWalk = TreeWalk.forPath(repository, getRelativePath(), tree);
+		}
+		return treeWalk;
+	}
+
+	private String getRelativePath() throws FileSystemException {
+		return rootName.getRelativeName(getName());
+	}
+
+	private boolean isRootDir() {
+		return getName().compareTo(rootName) == 0;
 	}
 }
---
+++ src/main/java/org/apache/commons/vfs2/provider/local/GitFileRandomAccessContent.java
@@ -0,0 +1,15 @@
+package org.apache.commons.vfs2.provider.local;
+
+import java.io.File;
+
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.util.RandomAccessMode;
+
+public class GitFileRandomAccessContent extends LocalFileRandomAccessContent {
+
+	public GitFileRandomAccessContent(File localFile, RandomAccessMode mode)
+			throws FileSystemException {
+		super(localFile, mode);
+	}
+
+}
